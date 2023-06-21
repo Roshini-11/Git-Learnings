c84f842848a91d08c66197d453f0cff0bd6e0a50
Kanthimathi-I-17 <kanthimathi.i@brimmatech.com>
2023-06-21 16:38:11 +0530

    a new int var a is declared and initilised to 1 and commit in feature 1

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,5 +1,6 @@
 public class NewClass {
     public void MyMethod(){
-        System.out.println("This is a java class file added by kanthimathi");
+        int a=1;
+        System.out.println("This is a java class file added by kanthimathi"+a);
     }
 }
