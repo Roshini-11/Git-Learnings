1fd1af425c33bc799ad77238369c1f9cdec06c72
roshini <roshini.s@brimmatech.com>
2023-06-21 16:40:11 +0530

    new integer added by roshini in new class file

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,5 +1,8 @@
 public class NewClass {
     public void MyMethod(){
+        int b=2;
+        System.out.println("This is a java file updated by Roshini");
+
         System.out.println("This is a java class file added by kanthimathi");
     }
 }
